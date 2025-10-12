1ee820b20fb3937e3509a4b7ee623cd9189e56a5
notmxm <maximbrasoveanu@gmail.com>
2025-10-12 23:48:02 +0200

    Ho aggiunto la pagina di creazione delle categorie

---
+++ Bulky/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+using Bulky.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: CategoryController
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public ActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
