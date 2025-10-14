a38dc7ca4cb78cc56ac299ab5ea84e8d9d2653a5
notmxm <53156674+notmxm@users.noreply.github.com>
2025-10-14 17:10:50 +0000

    Working category edit and delete with views

--- Bulky/Controllers/CategoryController.cs
+++ Bulky/Controllers/CategoryController.cs
@@ -41,7 +41,7 @@ namespace Bulky.Controllers
             }
             return View();
         }
-        
+
         public IActionResult Edit(int? id)
         {
             if (id == null || id == 0)
@@ -60,21 +60,54 @@ namespace Bulky.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            
-            if (obj.Name.ToLower() == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The Display Order cannot exacltly match the Name");
-            }
-            
 
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
-                return RedirectToAction("Index", "Category");    
+                return RedirectToAction("Index", "Category");
             }
             return View();
         }
 
+
+
+    public IActionResult Delete(int? id)
+        {   
+            
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+            
+        }
+
+
+
     }
+    
+        
 }
